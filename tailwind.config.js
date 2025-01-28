3f4d3ede9db56c3214392c1f4af826fe1eaba40e
Jupiter <jwestbard@gmail.com>
2025-01-28 15:55:39 -0500

    tailwind config stuff

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,29 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'mono': ['"Roboto Mono"', 'monospace']
+      }
+    }
   },
   plugins: [
     require('@tailwindcss/typography'),
+    function({ addComponents }) {
+      addComponents({
+        '.section': {
+          '@apply mb-12': {},
+        },
+        '.section-heading': {
+          '@apply text-2xl font-bold text-green-400': {},
+        },
+        '.project-border': {
+          '@apply border-l-4 border-green-400 pl-4': {},
+        },
+        '.project-title': {
+          '@apply text-xl font-bold text-green-400': {},
+        },
+      });
+    },
   ],
 }
